42e91fd7d656a4faf4483d060fbf8b84cd3123d3
Lynne Munini <muninilynne65@gmail.com>
2021-10-22 20:06:39 -0400

    Used exceptions

--- main.py
+++ main.py
@@ -1,33 +1,55 @@
-import pandas
-student_dict = {
-    "student": ["Angela", "James", "Lily"],
-    "score": [56, 76, 98]
-}
-
-# Looping through dictionaries:
-for (key, value) in student_dict.items():
-    # Access key and value
-    pass
-
-
-student_data_frame = pandas.DataFrame(student_dict)
-
-# Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    # Access index and row
-    # Access row.student or row.score
-    pass
+# import pandas
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
+# }
+#
+# # Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     # Access key and value
+#     pass
+#
+#
+# student_data_frame = pandas.DataFrame(student_dict)
+#
+# # Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     # Access index and row
+#     # Access row.student or row.score
+#     pass
+#
+# # Keyword Method with iterrows()
+# # {new_key:new_value for (index, row) in df.iterrows()}
+# nano = pandas.read_csv("nato_phonetic_alphabet.csv")
+# nano_dict = {row.letter: row.code for (index, row) in nano.iterrows()}
+# # print(nano_dict)
+# # TODO 1. Create a dictionary in this format:
+# # {"A": "Alfa", "B": "Bravo"}
+# name = input("Enter a word: ").upper()
+# # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# # Remove space in input
+# name = name.replace(" ", "")
+# name_list = [nano_dict[letter] for letter in name if letter in nano_dict]
+# print(name_list)
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
-nano = pandas.read_csv("nato_phonetic_alphabet.csv")
-nano_dict = {row.letter: row.code for (index, row) in nano.iterrows()}
-# print(nano_dict)
-# TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-name = input("Enter a word: ").upper()
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-# Remove space in input
-name = name.replace(" ", "")
-name_list = [nano_dict[letter] for letter in name if letter in nano_dict]
-print(name_list)
+
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+#TODO 1. Create a dictionary in this format:
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+# print(phonetic_dict)
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+while True:
+    word = input("\nEnter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Enter a letter in the alphabet please!")
+    else:
+        print(output_list)
+
